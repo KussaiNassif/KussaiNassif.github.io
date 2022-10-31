48f8b4119a29f40838153c5e42d406a4fc6e9ab6
Kussai Nassif <kussainassif01@gmail.com>
2022-10-31 19:40:06 +0100

    added alt to images

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ async function mapBurgers() {
     var itemElement = document.createElement('div');
     itemElement.classList.add('item');
     itemElement.innerHTML = `
-      <img class="item-img" src="${item.images[0].sm}" alt="burger">
+      <img class="item-img" src="${item.images[0].sm}" alt="${item.name}">
       <h1 class="item-title">${item.name}</h1>
       <h2>Price: ${item.price} SEK</h2>
       <p class="item-description">${item.desc}</p>
